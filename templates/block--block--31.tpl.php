b67181b175a1c45cf7f02a3d4efa441e67dfc4a1
David Guerrero <davidguerreromejias@gmail.com>
2016-09-19 15:29:59 +0200

    new file, 'aviso legal' first version

---
+++ templates/block--block--31.tpl.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for a block with bare minimum HTML.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728250
+ */
+?>
+
+<?php print render($title_prefix); ?>
+<?php if ($title): ?>
+  <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
+<?php endif; ?>
+<?php print render($title_suffix); ?>
+
+<div class="site-content-wrapper">
+  <div class="site-content">
+    <div class="row">
+      <div class="col-xs-12">
+        <ul class="list-inline u-mb u-fs-xsmall u-uppercase">
+          <li>
+            <a href="#" class="link">
+              <i class="u-color-turquesa icon-house"> </i>
+            </a>
+          </li>
+          <li class="items-separator--arrow">
+            <span class="u-color-gris">Aviso legal</span>
+          </li>
+        </ul>
+      </div>
+      <div class="col-xs-12 col-md-8 col-lg-9">
+        <div class="page-content-container u-mb++ ">
+          <div class="u-mb u-fs-h2 u-bold">Aviso Legal</div>
+          <div class="u-pv horizontal-line-separator-top u-fs-base">
+            <p class="u-mb-">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p class="u-mb-">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p class="u-mb-">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-12 col-md-4 col-lg-3">
+
+        <div class="u-mb+++">
+          <div class="u-pb- u-color-gris u-fs-xsmall u-uppercase u-semibold horizontal-line-separator-bottom">Información útil</div>
+          <ul class="u-color-turquesa u-fs-small u-semibold">
+            <li>
+              <a href="../prepara-viaje" class="link text-with-icon__content">
+                <img class="text-with-icon__icon" src="/assets/images/ruta-icon_small.svg" alt="Prepara tu viaje">
+                Prepara tu viaje
+              </a>
+            </li>
+            <li class="horizontal-line-separator-top">
+              <a href="../info-paises" class="link text-with-icon__content">
+                <img class="text-with-icon__icon" src="/assets/images/punto-icon_small.svg" alt="Información de los países">
+                Información de los paises
+              </a>
+            </li>
+            <li class="horizontal-line-separator-top horizontal-line-separator-bottom">
+              <a href="../preguntas-frecuentes" class="link text-with-icon__content">
+                <img class="text-with-icon__icon" src="/assets/images/dialogo-icon_small.svg" alt="Preguntas frecuentes">
+                Preguntas frecuentes
+              </a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</div>
