fe0f6eefd019866ed7780c5552910887b9d3fa75
David Guerrero <davidguerreromejias@gmail.com>
2016-09-29 10:01:56 +0200

    new styles v9

--- templates/block--block--28.tpl.php
+++ templates/block--block--28.tpl.php
@@ -18,23 +18,30 @@
 <ul class="list-inline display-only-up-md u-mb++">
   <div class="site-content-wrapper site-content-wrapper--centered">
       <li>
-        <a href="<?php print url('prepara-tu-viaje') ?>" class="u-block u-mh++" style="text-align: center;">
+        <a href="<?php print url('prepara-tu-viaje') ?>" class="u-block u-mh++ u-semibold u-lh-11 u-fs-small" style="text-align: center;">
           <img src="/sites/all/themes/zen/Nexos/assets/images/ruta-icon.svg" alt="Prepara tu viaje"><br/>
           <?php print t('Prepara <br/>tu viaje')?>
         </a>
       </li>
 
       <li>
-        <a href="<?php print url('info-paises') ?>" class="u-block u-mh++" style="text-align: center;">
+        <a href="<?php print url('info-paises') ?>" class="u-block u-mh++ u-semibold u-lh-11 u-fs-small" style="text-align: center;">
           <img src="/sites/all/themes/zen/Nexos/assets/images/punto-icon.svg" alt="Información de los países"><br/>
           <?php print t('Información <br/>de los países')?>
         </a>
       </li>
 
       <li>
-        <a href="<?php print url('preguntas-frecuentes') ?>" class="u-block u-mh++" style="text-align: center;">
+        <a href="<?php print url('preguntas-frecuentes') ?>" class="u-block u-mh++ u-semibold u-lh-11 u-fs-small" style="text-align: center;">
           <img src="/sites/all/themes/zen/Nexos/assets/images/dialogo-icon.svg" alt="Preguntas frecuentes"><br/>
           <?php print t('Preguntas <br/>frecuentes')?>
+        </a>
+      </li>
+
+      <li>
+        <a href="<?php print url('portadilla-investigadores') ?>" class="u-block u-mh++ u-semibold u-lh-11 u-fs-small" style="text-align: center;">
+          <img src="/sites/all/themes/zen/Nexos/assets/images/bombilla-icon.svg" alt="Campus Plus"><br/>
+          <?php print t('Campus <br/> Plus')?>
         </a>
       </li>
     </div>
