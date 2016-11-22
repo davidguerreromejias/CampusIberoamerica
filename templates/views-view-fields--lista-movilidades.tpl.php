3051616903f02b7fbaf202ccd956c691871b282d
David Guerrero <davidguerreromejias@gmail.com>
2016-11-22 12:59:45 +0100

    conditional fields working in admin

--- templates/views-view-fields--lista-movilidades.tpl.php
+++ templates/views-view-fields--lista-movilidades.tpl.php
@@ -34,7 +34,6 @@
 	    	<div>
 	    	<td style="padding: 6px;"><?php print $field->content; ?>
 	    		<!--"https://movia.fib.upc.edu:8444/es/node/add/movilidad"-->
-			  <a href="https://movia.fib.upc.edu:8444/es/node/<?php print $row->nid;?>/delete" class="btn btn--remove"><i style="margin-right: 5px;" class="fa fa-trash" aria-hidden="true"></i>Borrar</a>
 			  <a href="https://movia.fib.upc.edu:8444/es/node/<?php print $row->nid;?>/edit" class="btn btn--edit"><i style="margin-right: 5px;" class="fa fa-pencil-square-o" aria-hidden="true"></i>Editar</a>
 			  
 	    	</td>
