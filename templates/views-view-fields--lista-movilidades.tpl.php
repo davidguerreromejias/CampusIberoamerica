e4f55868ba50175d7b55df52b0244c4569af88a2
David Guerrero <davidguerreromejias@gmail.com>
2016-11-23 09:47:04 +0100

    show smth depending role

--- templates/views-view-fields--lista-movilidades.tpl.php
+++ templates/views-view-fields--lista-movilidades.tpl.php
@@ -34,8 +34,10 @@
 	    	<div>
 	    	<td style="padding: 6px;"><?php print $field->content; ?>
 	    		<!--"https://movia.fib.upc.edu:8444/es/node/add/movilidad"-->
+	    	<?php if (is_array($user->roles) && in_array('administrator', $user->roles)) { ?>
+	    	  <a href="https://movia.fib.upc.edu:8444/es/node/<?php print $row->nid;?>/delete" class="btn btn--remove"><i style="margin-right: 5px;" class="fa fa-trash" aria-hidden="true"></i>Borrar</a>
+	    	<?php } ?>
 			  <a href="https://movia.fib.upc.edu:8444/es/node/<?php print $row->nid;?>/edit" class="btn btn--edit"><i style="margin-right: 5px;" class="fa fa-pencil-square-o" aria-hidden="true"></i>Editar</a>
-			  
 	    	</td>
 	    	</div>
 	    </tr>
