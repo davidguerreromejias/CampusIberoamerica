680cddecc0abaa43ec3a103dd049050e280dd240
ricard.meyerhofer <ricard.meyerhofer@fib.upc.edu>
2017-01-11 12:58:44 +0100

    js pushejat de fa uns dies, funciona correctament, no sé per què no va bé si ho modifico

--- js/index.js
+++ js/index.js
@@ -1114,14 +1114,13 @@ y(function ($) {
 
   // Convertimos en personalizados los select del buscador
   $('.js-buscador-select').each(function (index, item) {
-	
     // Construimos un select personalizado para cada combo
     var select = new Select({
       el: item,
       className: 'p-buscador_select',
       scrollParent: $overlay.find('.js-overlay-page-content')[0]
     });
-	
+
     // Ajustamos la anchura de los elementos al tamaño del select (target)
     select.on('open', function () {
       $(select.drop).width($(select.target).outerWidth(true));
@@ -1130,7 +1129,7 @@ y(function ($) {
     // Cerramos el select si se redimensiona la pantalla
     $(window).resize(function () {
       if (select.isOpen()) {
-        select.close();
+        // select.close();
       }
     });
   });
@@ -1216,25 +1215,6 @@ jQuery(document).ready(function ($) {
   };
 
   $mapa.mapster(config);
-});
-jQuery(document).ready(function ($) {
-  $( ".cross" ).hide();
-  $( ".menu" ).hide();
-  $( ".hamburger" ).click(function() {
-    $( ".menu" ).slideToggle( "slow", function() {
-      $( ".hamburger" ).hide();
-      $( ".cross" ).show();
-    });
-  });
-
-  $( ".cross" ).click(function() {
-    $( ".menu" ).slideToggle( "slow", function() {
-      $( ".cross" ).hide();
-      $( ".hamburger" ).show();
-    });
-  });
-)};
-
 });
 
 
