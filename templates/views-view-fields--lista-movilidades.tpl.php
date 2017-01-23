1da947f399469e24860990d06518c6f34edabacb
ricard.meyerhofer <ricard.meyerhofer@fib.upc.edu>
2017-01-23 10:48:38 +0100

    modificat estil

--- templates/views-view-fields--lista-movilidades.tpl.php
+++ templates/views-view-fields--lista-movilidades.tpl.php
@@ -32,13 +32,20 @@
 	    <?php print $field->label_html; ?>
 	    <tr>
 	    	<div>
-	    	<td style="padding: 6px;"><?php print $field->content; ?>
-	    		<!--"https://movia.fib.upc.edu:8444/es/node/add/movilidad"-->
-	    	<?php if (is_array($user->roles) && (in_array('admin SEGIB', $user->roles) || in_array('administrator', $user->roles))) { ?>
-	    	  <a href="/es/node/<?php print $row->nid;?>/delete" class="btn btn--remove"><i style="margin-right: 5px;" class="fa fa-trash" aria-hidden="true"></i>Borrar</a>
-	    	<?php } ?>
-			  <a href="/es/node/<?php print $row->nid;?>/edit" class="btn btn--edit"><i style="margin-right: 5px;" class="fa fa-pencil-square-o" aria-hidden="true"></i>Editar</a>
-	    	</td>
+		    	<td style="padding: 6px;"> 
+		    		<?php
+		    			$aux = $row->nid;
+			    		print $aux;
+		    			print " - ";
+			    		print $field->content; 
+	    		 	?>
+
+		    		<!--"https://movia.fib.upc.edu:8444/es/node/add/movilidad"-->
+		    	<?php if (is_array($user->roles) && (in_array('admin SEGIB', $user->roles) || in_array('administrator', $user->roles))) { ?>
+		    	  <a href="/es/node/<?php print $row->nid;?>/delete" class="btn btn--remove"><i style="margin-right: 5px;" class="fa fa-trash" aria-hidden="true"></i>Borrar</a>
+		    	<?php } ?>
+				  <a href="/es/node/<?php print $row->nid;?>/edit" class="btn btn--edit"><i style="margin-right: 5px;" class="fa fa-pencil-square-o" aria-hidden="true"></i>Editar</a>
+		    	</td>
 	    	</div>
 	    </tr>
 	  <?php print $field->wrapper_suffix; ?>
